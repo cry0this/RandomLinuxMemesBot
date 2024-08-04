604b669b1998c6dc9b0ef2d2c6c5e87eb02ee9b9
cry0this <cry0this@gmail.com>
2024-08-04 14:47:02 +0500

    fix permissions

--- internal/discord/commands.go
+++ internal/discord/commands.go
@@ -4,13 +4,10 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var permissions int64 = discordgo.PermissionSendMessages & discordgo.PermissionAttachFiles
-
 var commands = []*discordgo.ApplicationCommand{
 	{
-		Name:                     "get-random-linux-meme",
-		Description:              "Get random linux meme",
-		DefaultMemberPermissions: &permissions,
+		Name:        "get-random-linux-meme",
+		Description: "Get random linux meme",
 	},
 }
 
